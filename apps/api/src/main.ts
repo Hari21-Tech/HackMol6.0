22873ea20daa40aff5655aea1d9eda2504c2c0e9
Aargh Rai <shubh2007rai@gmail.com>
2025-04-10 15:14:17 +0530

    wtf this just works????????????????????????????
    also fuck auto sync

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,41 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
 import * as path from 'path';
 
-const app = express();
+import database, { ensureTables } from '@hackmol/database'
+import { AI } from './ai';
 
+ensureTables()
+
+new AI()
+  .onOpen(() => {return})
+  .onClose(() => {return});
+
+const app = express();
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+const router = express.Router()
+router.get('/', (req, res) => {
+  res.send({ message: 'Working' });
 });
+router.get('/fuck', (req, res) => {
+  res.send({
+    fuck: database.shops.createShop({
+      name:'Fuck',
+      description: 'Get your fucks here! Fresh fucks',
+      position: {
+        latitude: 1.0,
+        longitude: 1.0,
+      },
+      image: 'https://www.google.com/imgres?q=fuck%20off%20image&imgurl=https%3A%2F%2Fvoodooneon.com%2Fcdn%2Fshop%2Ffiles%2Ffuck-off-cool-white.jpg%3Fv%3D1699928832%26width%3D1445&imgrefurl=https%3A%2F%2Fvoodooneon.com%2Fproducts%2Ffuck-off&docid=96YF45Vy5GKCVM&tbnid=K9QzJaq1OZ6K2M&vet=12ahUKEwjhquvbic2MAxWBzTgGHZfCF3oQM3oECBcQAA..i&w=1445&h=1445&hcb=2&ved=2ahUKEwjhquvbic2MAxWBzTgGHZfCF3oQM3oECBcQAA',
+      current_occupancy: 12,
+      total_occupancy: 20,
+    })
+  });
+})
 
-const port = process.env.PORT || 3333;
+app.use('/api', router);
+const port = Number(process.env.PORT);
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
-server.on('error', console.error);
+server.on('error', console.error.bind(null, "Server Error: "));
---
+++ libs/database/src/lib/parking_spot.ts
@@ -0,0 +1,76 @@
+export const tableData = [
+    'id SERIAL PRIMARY KEY',
+    'parking_id INTEGER REFERENCES parking(id) ON DELETE CASCADE',
+    'floor TEXT NOT NULL',
+    'spot TEXT NOT NULL',
+    'UNIQUE (parking_id, floor, spot)',
+];
+
+//! IMPORTANT: ASSUME FAKE DATA OF EXACTLY 100 ROWS
+const alphaChars = "qwertyuioplkjhgfdsazxcvbnm";
+export const fakerData: () => ParkingSpotData = () => ({
+    parking_id: Math.round(Math.random() * 100),
+    floor: Math.round(Math.random() * 4).toString(),
+    spot: alphaChars[Math.round(Math.random() * 26)].toUpperCase() + Math.round(Math.random() * 69).toString()
+})
+
+const CREATE_QUERY = 'INSERT INTO parking_spot (parking_id, floor, spot) VALUES ($1, $2, $3) ON CONFLICT (parking_id, position_label) DO NOTHING;';
+const GET_QUERY = 'SELECT * FROM parking_spot WHERE parking_id=$1;';
+const DELETE_QUERY = 'DELETE FROM parkings WHERE id=$1;';
+const DELETE_ALL_QUERY = 'DELETE FROM parkings WHERE parking_id=$1;';
+
+import { Pool } from "pg";
+import { Result, DatabaseReturn } from "./result.js";
+
+type ParkingSpotEntry = {
+  id: number;
+  parking_id: number;
+  floor: string;
+  spot: string;
+};
+type ParkingSpotData = Omit<ParkingSpotEntry, 'id'>;
+
+function getParkingSpots(
+    pool: Pool,
+    parking_id: number,
+): DatabaseReturn<ParkingSpotEntry> {
+    return Result(async () => {
+        return await pool.query<ParkingSpotEntry>(GET_QUERY, [parking_id]);
+    });
+}
+
+function addParkingSpot(
+    pool: Pool,
+    { parking_id, floor, spot }: ParkingSpotData,
+): DatabaseReturn<ParkingSpotEntry> {
+    return Result(async () => {
+        return await pool.query<ParkingSpotEntry>(CREATE_QUERY, [parking_id, floor, spot]);
+    });
+}
+
+function removeParkingSpot(
+    pool: Pool,
+    parking_spot_id: number,
+): DatabaseReturn<ParkingSpotEntry> {
+    return Result(async () => {
+        return await pool.query<ParkingSpotEntry>(DELETE_QUERY, [parking_spot_id]);
+    });
+}
+
+function removeAllOfParkingArea(
+    pool: Pool,
+    parking_id: number,
+): DatabaseReturn<ParkingSpotEntry> {
+    return Result(async () => {
+        return await pool.query<ParkingSpotEntry>(DELETE_ALL_QUERY, [parking_id]);
+    });
+}
+
+export function bundle(pool: Pool) {
+    return {
+        getParkingSpots: getParkingSpots.bind(null, pool),
+        addParkingSpot: addParkingSpot.bind(null, pool),
+        removeParkingSpot: removeParkingSpot.bind(null, pool),
+        removeAllOfParkingArea: removeAllOfParkingArea.bind(null, pool),
+    };
+}
